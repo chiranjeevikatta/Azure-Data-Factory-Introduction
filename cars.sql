c068427cf4497377b3c20e4cc5bfa3eb53bebe58
chiranjeevikatta <64351663+chiranjeevikatta@users.noreply.github.com>
2021-03-22 17:54:11 +0530

    Create cars.sql

---
+++ cars.sql
@@ -0,0 +1,15 @@
+Create table cars
+(
+Name varchar(100),
+mpg decimal(18,3),
+cyl decimal(18,3),
+disp decimal(18,3),
+hp decimal(18,3),
+drat decimal(18,3),
+wt decimal(18,3),
+qsec decimal(18,3),
+vs decimal(18,3),
+am decimal(18,3),
+gear decimal(18,3),
+carb decimal(18,3)
+)
